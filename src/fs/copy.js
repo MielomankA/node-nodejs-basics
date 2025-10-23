d0f78b7bc3a726619321ec3c58e31015eda92bbc
MielomankA <mielomanka@mail.ru>
2025-10-23 21:18:16 +0300

    feat: implement fs/copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+
+const folderUrl = new URL("./files", import.meta.url);
+const folderUrlCopy = new URL("./files_copy", import.meta.url);
+
 const copy = async () => {
-  // Write your code here
+  if (!fs.existsSync(folderUrl)) {
+    throw Error('FS operation failed');
+  }
+
+  if (fs.existsSync(folderUrlCopy)) {
+    throw Error('FS operation failed');
+  } else {
+    fs.cp(folderUrl, folderUrlCopy, { recursive: true }, (err) => {
+      if (err) {
+        console.error('Error copying directory:', err);
+      } else {
+        console.log('Directory copied successfully!');
+      }
+    });
+  }
 };
 
 await copy();
