c934afb059091eb9a851c4942fe4926921de520c
MielomankA <mielomanka@mail.ru>
2025-10-23 21:29:27 +0300

    refactor: change fs/copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -4,21 +4,17 @@ const folderUrl = new URL("./files", import.meta.url);
 const folderUrlCopy = new URL("./files_copy", import.meta.url);
 
 const copy = async () => {
-  if (!fs.existsSync(folderUrl)) {
+  if (!fs.existsSync(folderUrl) || fs.existsSync(folderUrlCopy)) {
     throw Error('FS operation failed');
   }
 
-  if (fs.existsSync(folderUrlCopy)) {
-    throw Error('FS operation failed');
-  } else {
-    fs.cp(folderUrl, folderUrlCopy, { recursive: true }, (err) => {
-      if (err) {
-        console.error('Error copying directory:', err);
-      } else {
-        console.log('Directory copied successfully!');
-      }
-    });
-  }
+  fs.cp(folderUrl, folderUrlCopy, { recursive: true }, (err) => {
+    if (err) {
+      console.error('Error copying directory:', err);
+    } else {
+      console.log('Directory copied successfully!');
+    }
+  });
 };
 
 await copy();
