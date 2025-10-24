57276ffeff5f15fb468f9592f6cd842307014b72
MielomankA <mielomanka@mail.ru>
2025-10-24 22:28:09 +0300

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+const RSS_PREFIX = 'RSS_'
+
 const parseEnv = () => {
-  // Write your code here
+  const regex = new RegExp(`^${RSS_PREFIX}[A-Za-z0-9_]+$`);
+
+  const rssVariables = Object.entries(process.env).filter(([key]) => {
+    return regex.test(key);
+  }).map(envObject => {
+    return `${envObject[0]}=${envObject[1]}`;
+  }).join('; ');
+
+  console.log(rssVariables);
 };
 
 parseEnv();
