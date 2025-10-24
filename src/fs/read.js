9aef601f8fe27d8fe8d1993796e8a8fb6de48bb2
MielomankA <mielomanka@mail.ru>
2025-10-24 21:22:08 +0300

    feat: implement fs/read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+
+const fileUrl = new URL("./files/fileToRead.txt", import.meta.url);
+
 const read = async () => {
-  // Write your code here
+  if (!fs.existsSync(fileUrl)) {
+    throw Error('FS operation failed');
+  }
+
+  fs.readFile(fileUrl, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+
+    console.log(data);
+  })
 };
 
 await read();
