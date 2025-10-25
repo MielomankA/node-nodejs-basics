8b3d7f49634f8a467a5845caa33c2b1c8427c5f2
MielomankA <mielomanka@mail.ru>
2025-10-25 22:38:28 +0300

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream, existsSync } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
 const compress = async () => {
-  // Write your code here
+  const fileUrl = new URL("./files/fileToCompress.txt", import.meta.url);
+  const fileArchiveUrl = new URL("./files/archive.gz", import.meta.url);
+  const source = createReadStream(fileUrl);
+  const zipper = createGzip();
+  const destination = createWriteStream(fileArchiveUrl);
+
+  await pipeline(
+    source,
+    zipper,
+    destination
+  );
+
+  console.log('Pipeline succeeded.');
 };
 
 await compress();
