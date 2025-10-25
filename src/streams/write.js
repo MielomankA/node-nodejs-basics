04fc46b8599ea8508df84d21269a15ae081cc610
MielomankA <mielomanka@mail.ru>
2025-10-25 20:05:42 +0300

    feat: implement stream/write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import process from 'process';
+
 const write = async () => {
-  // Write your code here
+  const fileUrl = new URL("./files/fileToWrite.txt", import.meta.url);
+  const outputFile = fs.createWriteStream(fileUrl);
+  const input = process.stdin;
+
+  if (!fs.existsSync(fileUrl)) {
+    throw Error('The file to write does not exist');
+  }
+
+  console.log('*** Please start writing something and press Enter, then Ctrl + C to finish ***');
+  input.pipe(outputFile);
+
+  process.on('SIGINT', () => {
+    console.log('\nWriting finished');
+    input.unpipe(outputFile);
+    outputFile.end();
+  });
 };
 
 await write();
