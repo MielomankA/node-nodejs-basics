60174d5068ed66fb4ebfa7fbe07a2b1bdcb5bb13
MielomankA <mielomanka@mail.ru>
2025-10-25 21:44:03 +0300

    feat: implement stream/transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+import process from 'process';
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-  // Write your code here
+  const input = process.stdin;
+  const output = process.stdout;
+
+  console.log('*** Please start writing something and press Enter, then Ctrl + C to finish ***');
+
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedOutput = chunk.toString().split('').reverse().join('');
+
+      callback(null, reversedOutput);
+    }
+  });
+
+  input.pipe(reverseTransform).pipe(output);
+
+  process.on('SIGINT', () => {
+    input.unpipe(reverseTransform);
+  });
 };
 
 await transform();
