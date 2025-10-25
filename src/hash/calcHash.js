103217ce26bf7d9eb6ee1b3494bee8718c6fe727
MielomankA <mielomanka@mail.ru>
2025-10-25 14:17:02 +0300

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import сrypto from 'node:crypto';
+import process from 'process';
+
 const calculateHash = async () => {
-  // Write your code here
+  const fileUrl = new URL("./files/fileToCalculateHashFor.txt", import.meta.url);
+  const input = fs.createReadStream(fileUrl);
+  const hash = сrypto.createHash('sha256');
+
+  hash.setEncoding('hex');
+  input.pipe(hash).pipe(process.stdout);
+  input.on('end', () => process.stdout.write('\n'));
 };
 
 await calculateHash();
