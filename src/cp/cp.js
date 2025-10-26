3272da8f54705a230d050d8ca865e1878d187980
MielomankA <mielomanka@mail.ru>
2025-10-26 21:10:49 +0300

    feat: implement cp function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const fileUrl = new URL("./files/script.js", import.meta.url);
+  const filePath = fileURLToPath(fileUrl);
+  const command = process.execPath;
+
+  const allArguments = [filePath, ...args];
+
+  const options = {
+    stdio: 'inherit'
+  };
+
+  const child = spawn(command, allArguments, options);
+
+  child.on('error', (err) => {
+    console.error('Error in child process', err);
+  });
+
+  return child;
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', 'someArgument3']);
