a336251f40d6b5b87cde769b0c67b80668c67924
MielomankA <mielomanka@mail.ru>
2025-10-26 19:10:40 +0300

    feat: implement worker threads functionality

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
+import { parentPort, workerData } from 'node:worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-  // This function sends result of nthFibonacci computations to main thread
+  const { index, value } = workerData;
+  try {
+    const result = nthFibonacci(value);
+
+    parentPort.postMessage(result);
+  } catch (error) {
+    parentPort.postMessage({ index, error: error.message });
+  }
 };
 
 sendResult();
